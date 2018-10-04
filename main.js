fb2d283d20f985242112ef7c2a3c9a37d9048941
Berkley Horan <horans11@gmail.com>
2018-10-04 08:07:42 -0600

    I created a base website with a pull down bar and

---
+++ main.js
@@ -0,0 +1,11 @@
+
+function resized(){
+    
+   
+
+}
+
+function shrink(){
+    document.getElementById("banner").style.height = "0%";
+    document.getElementById("banner").style.position = "relative";
+}
